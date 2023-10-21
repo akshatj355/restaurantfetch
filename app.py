ebe6ea567a6349dda598ac4c5eed0d730744a909
Akshat Jain <akshatj355@gmail.com>
2023-10-21 18:37:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,70 @@ class RestaurantRecommendationSystem:
 
         return restaurants
 
-db = mysql.connector.connect(host='restaurantfetch-server', user='nweikyohdm', password='OA423FQ81XB3WSLG$', db='restaurants')
+# Create a MySQL connection without specifying a database
+db = mysql.connector.connect(host='localhost', user='root', password='Akshatj@355')
+cursor = db.cursor()
+
+# Drop the 'restaurants' database if it already exists
+drop_database_query = "DROP DATABASE IF EXISTS restaurants;"
+cursor.execute(drop_database_query)
+
+# Create the 'restaurants' database
+create_database_query = "CREATE DATABASE restaurants;"
+cursor.execute(create_database_query)
+
+# Switch to the 'restaurants' database
+use_database_query = "USE restaurants;"
+cursor.execute(use_database_query)
+
+create_table_query = """
+CREATE TABLE IF NOT EXISTS restaurants (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  style VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  openHour TIME NOT NULL,
+  closeHour TIME NOT NULL,
+  vegetarian BOOL NOT NULL,
+  delivery BOOL NOT NULL,
+  PRIMARY KEY (id)
+);
+"""
+
+# Check if the 'restaurants' table exists before attempting to create it
+cursor.execute(create_table_query)
+
+# Reconnect to the 'restaurants' database
+db = mysql.connector.connect(host='localhost', user='root', password='Akshatj@355', database='restaurants')
+cursor = db.cursor()
+
+# Insert data into the database
+insert_query = """
+INSERT INTO restaurants (name, style, address, openHour, closeHour, vegetarian, delivery) VALUES
+  ('Pizza Hut', 'Italian', 'Wherever Street 99, Somewhere', '09:00', '23:00', TRUE, TRUE),
+  ('Dominos', 'Italian', 'Any Street 100, Somewhere', '10:00', '22:00', TRUE, TRUE),
+  ('Subway', 'American', 'Random Street 101, Somewhere', '11:00', '21:00', TRUE, TRUE),
+  ('McDonald''s', 'American', 'Random Street 102, Somewhere', '12:00', '20:00', FALSE, TRUE),
+  ('Burger King', 'American', 'Random Street 103, Somewhere', '13:00', '19:00', FALSE, TRUE),
+  ('KFC', 'American', 'Random Street 104, Somewhere', '14:00', '18:00', FALSE, TRUE),
+  ('Starbucks', 'Coffee', 'Random Street 105, Somewhere', '15:00', '17:00', TRUE, TRUE),
+  ('Costa Coffee', 'Coffee', 'Random Street 106, Somewhere', '16:00', '16:00', TRUE, TRUE),
+  ('Indian Curry House', 'Indian', 'Random Street 107, Somewhere', '17:00', '15:00', TRUE, TRUE),
+  ('Chinese Takeaway', 'Chinese', 'Random Street 108, Somewhere', '18:00', '14:00', TRUE, TRUE),
+  ('Mexican Restaurant', 'Mexican', 'Random Street 109, Somewhere', '19:00', '13:00', TRUE, TRUE),
+  ('Thai Restaurant', 'Thai', 'Random Street 110, Somewhere', '20:00', '12:00', TRUE, TRUE),
+  ('Japanese Restaurant', 'Japanese', 'Random Street 111, Somewhere', '21:00', '11:00', TRUE, TRUE),
+  ('Korean Restaurant', 'Korean', 'Random Street 112, Somewhere', '22:00', '10:00', TRUE, TRUE),
+  ('Vietnamese Restaurant', 'Vietnamese', 'Random Street 113, Somewhere', '23:00', '09:00', TRUE, TRUE),
+  ('Vegan Restaurant', 'Vegan', 'Random Street 114, Somewhere', '00:00', '08:00', TRUE, TRUE),
+  ('Gluten-Free Restaurant', 'Gluten-Free', 'Random Street 115, Somewhere', '01:00', '07:00', TRUE, TRUE),
+  ('Halal Restaurant', 'Halal', 'Random Street 116, Somewhere', '02:00', '06:00', TRUE, TRUE),
+  ('Kosher Restaurant', 'Kosher', 'Random Street 117, Somewhere', '03:00', '05:00', TRUE, TRUE),
+  ('24-Hour Restaurant', '24-Hour', 'Random Street 118, Somewhere', '04:00', '04:00', TRUE, TRUE);
+"""
+
+cursor.execute(insert_query)
+db.commit()
 recommendation_system = RestaurantRecommendationSystem(db)
 
 app = Flask(__name__)
